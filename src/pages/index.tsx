8585eb0910dfa3a4b962ce410f71dc6b316bff37
Boris Tsao <boris.tsao@gmail.com>
2024-04-10 20:50:27 -0700

    adding in client data info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
-import { AddClientContainer } from "~/components/AddClientContainer";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+  const usersToClients = api.client.getUsersToClients.useQuery();
+
+  console.log(usersToClients.data);
 
   return (
     <>
@@ -16,32 +18,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center ">
-        <AddClientContainer />
+        Hello world
       </main>
     </>
   );
 }
 
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-  console.log(sessionData);
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
+// function AuthShowcase() {
+//   const { data: sessionData } = useSession();
+//   console.log(sessionData);
+//   const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+//     undefined, // no input
+//     { enabled: sessionData?.user !== undefined },
+//   );
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-4">
+//       <p className="text-center text-2xl text-white">
+//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+//         {secretMessage && <span> - {secretMessage}</span>}
+//       </p>
+//       <button
+//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+//         onClick={sessionData ? () => void signOut() : () => void signIn()}
+//       >
+//         {sessionData ? "Sign out" : "Sign in"}
+//       </button>
+//     </div>
+//   );
+// }
--- src/server/api/routers/clients.ts
+++ src/server/api/routers/clients.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 import {
@@ -5,7 +6,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import { clients } from "~/server/db/schema";
+import { clients, usersToClients } from "~/server/db/schema";
 
 export const clientRouter = createTRPCRouter({
   hello: publicProcedure
@@ -31,6 +32,14 @@ export const clientRouter = createTRPCRouter({
         baseCurrency: input.baseCurrency,
       });
     }),
+
+  getUsersToClients: protectedProcedure.query(async ({ ctx }) => {
+    const listOfClients = await ctx.db
+      .select()
+      .from(clients)
+      .innerJoin(usersToClients, eq(usersToClients.clientId, clients.id));
+    return listOfClients;
+  }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
