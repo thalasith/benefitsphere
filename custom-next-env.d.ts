6abafa772f675683a3004494a37828ae623d94dc
Boris Tsao <boris.tsao@gmail.com>
2024-04-20 07:36:18 -0700

    really trying to fix it now

--- custom-next-env.d.ts
+++ custom-next-env.d.ts
@@ -1,4 +1,3 @@
-declare module "*.svg" {
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
-}
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
