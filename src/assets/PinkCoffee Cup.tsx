f62f413573174c03f8ae214e6c4123740919e060
Boris Tsao <boris.tsao@gmail.com>
2024-05-11 22:13:54 -0700

    trying to fix it all again

--- src/assets/PinkCoffee Cup.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PinkCoffee Cup: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="#bed3e4" d="M25 8H7l2 22h14l2-22"/><path fill="#ee3d8b" d="M26 8H6V5h20v3M23 2H9L8 5h16l-1-3"/><path fill="#de0f4c" d="M24 5H8L7 8h18l-1-3"/><path fill="#ee3d8b" d="M12 17a4 4 0 1 1 4 4 4 4 0 0 1-4-4"/><path fill="#0082c8" d="M24.18 17H20a4 4 0 0 1-8 0H7.82L9 30h14l1.18-13"/><path fill="#00267a" d="M20 17h-8a4 4 0 0 0 8 0"/></svg>
-);
-
-export default PinkCoffee Cup;
